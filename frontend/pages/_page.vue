ef836eb380fd48825c473cd88559aae7b99a824f
examp1 <bebe4kaxd@gmail.com>
2023-09-07 13:20:50 +0300

    routing

---
+++ frontend/pages/_page.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+    <sections-render :props-data="apiData.constructor"></sections-render>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { getPageBySlug } from '~/services/pageService'
+import sectionsRender from '~/components/sectionsRender.vue'
+export default {
+  name: 'HomePage',
+  components: { sectionsRender },
+  async asyncData(ctx) {
+    const data = await getPageBySlug(ctx, ctx.params.page)
+    return {
+      apiData: data,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getMode: 'checkMode/getMode',
+    }),
+  },
+}
+</script>
+<style lang="scss" scoped>
+</style>
