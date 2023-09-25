08e90c7bb3ed03a47c28ce18dbf5dd90cb17f0a3
examp1 <bebe4kaxd@gmail.com>
2023-09-25 17:35:49 +0300

    nuxt generate

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -16,58 +16,15 @@ export default defineNuxtConfig({
         link: [{ rel: "icon", href: "/favicon.ico" }]
     },
 
-    css: [
-        // '~/assets/scss/main.scss',
-        // ... (остальные стили)
-    ],
-
-    //   plugins: [
-    //     '~/mixins/utils.js',
-    //     '~/mixins/seoMixin.js',
-    //     '~/plugins/global.js',
-    //     '~/filters/index.js'
-    //   ],
-
-    buildModules: ["@nuxtjs/eslint-module"],
-
-    // Для модулей может потребоваться проверка на их совместимость с Nuxt 3 или их альтернативы
-    //   modules: [
-    //     '@nuxtjs/device',
-    //     // Style Resources module может потребоваться обновление для Nuxt 3
-    //     '@nuxtjs/style-resources',
-    // '@nuxtjs/axios',
-    //     'cookie-universal-nuxt',
-    //     '@nuxtjs/i18n'
-    //   ],
-
-    i18n: {
-        defaultLocale: "uk",
-        locales: [
-            {
-                code: "uk",
-                file: "uk.json"
-            },
-            {
-                code: "en",
-                file: "en.json"
-            }
-        ],
-        langDir: "~/locales/"
-    },
-
-    build: {
-        publicPath: process.env.NODE_ENV === "production" ? "app/" : "_nuxt/"
-        // Настройки транспиляции может потребоваться обновить или изменить
-    },
-
-    // Для generate и router может потребоваться дополнительная настройка или обновление
-    generate: {
-        dir: process.env.NODE_ENV === "production" ? "../public/assets" : "dist"
-    },
+    //   dir: {
+    //     public: process.env.NODE_ENV === 'production' ? '../public/assets' : 'dist'
+    //   },
+    //   buildDir: process.env.NODE_ENV === 'production' ? '../public/assets/app/' : '.nuxt/',
+    build: {},
 
     router: {
-        middleware: ["getMenusAndSettings", "toLowerCase"],
-        linkActiveClass: "activeLink"
+        // middleware: ['getMenusAndSettings', 'toLowerCase'],
+        // linkActiveClass: 'activeLink',
         // И далее ваш код расширения маршрутов...
     }
 });
--- frontend/updateNuxtBlade.js
+++ frontend/updateNuxtBlade.js
@@ -1,20 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-const sourcePath = path.resolve(__dirname, '../public/assets/200.html');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourcePath = path.resolve(__dirname, './.output/public/index.html');
 const targetPath = path.resolve(__dirname, '../resources/views/front/home/frontend.blade.php');
 
-// Копирование файла
-fs.copyFile(sourcePath, targetPath, (err) => {
-    if (err) {
-        console.error('Ошибка при копировании файла:', err);
-        return;
-    }
-
+const copyAndUpdateFile = async () => {
+  try {
+    await fs.copyFile(sourcePath, targetPath);
     console.log(`Файл успешно скопирован в ${targetPath}`);
 
-    // Теперь можно безопасно читать и изменять целевой файл
-    const fileContent = fs.readFileSync(targetPath, 'utf8');
+    const fileContent = await fs.readFile(targetPath, 'utf8');
     const updatedContent = fileContent.replace(/\/app\//g, '/public/assets/app/');
-    fs.writeFileSync(targetPath, updatedContent);
-});
+    await fs.writeFile(targetPath, updatedContent);
+  } catch (err) {
+    console.error('Ошибка при копировании файла:', err);
+  }
+};
+
+copyAndUpdateFile();
