b396c472c1a95a320ed998c929c5c71d3c7b2246
examp1 <bebe4kaxd@gmail.com>
2023-09-25 16:26:50 +0300

    nuxt 3

--- frontend/filters/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-Vue.filter('zeroPad', function (value) {
-  if (typeof value !== 'number') {
-    return value;
-  }
-
-  return value < 10 ? '0' + value : value.toString();
-});
--- frontend/middleware/toLowerCase.js
+++
@@ -1,9 +0,0 @@
-// middleware/toLowerCase.js
-export default function ({ route, redirect }) {
-  const path = route.fullPath;
-  const lowerCasePath = path.toLowerCase();
-
-  if (path !== lowerCasePath && !path.includes('s-login')) {
-    redirect(301, lowerCasePath);
-  }
-}
