c85cd3dcdb5da79d90a03d6d652cf360b0eb2494
examp1 <bebe4kaxd@gmail.com>
2023-10-03 17:23:53 +0300

    123

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,29 +2,5 @@ export default defineNuxtConfig({
     // SSR можно настроить через server
     // https://v3.nuxtjs.org/docs/usage/ssr
 
-    ssr: false,
-
-    head: {
-        title: "spa-ssr-template",
-        htmlAttrs: {
-            lang: "en"
-        },
-        meta: [
-            { charset: "utf-8" }
-            // ... (остальные мета-теги остаются без изменений)
-        ],
-        link: [{ rel: "icon", href: "/favicon.ico" }]
-    },
-
-    //   dir: {
-    //     public: process.env.NODE_ENV === 'production' ? '../public/assets' : 'dist'
-    //   },
-    //   buildDir: process.env.NODE_ENV === 'production' ? '../public/assets/app/' : '.nuxt/',
-    build: {},
-
-    router: {
-        // middleware: ['getMenusAndSettings', 'toLowerCase'],
-        // linkActiveClass: 'activeLink',
-        // И далее ваш код расширения маршрутов...
-    }
+    ssr: false
 });
