3180ee5ee1e215a4c8b66070c24b17692233add0
examp1 <bebe4kaxd@gmail.com>
2023-10-16 22:17:03 +0300

    iamges + client pokaz

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="page">
+        <app-banners-sections></app-banners-sections>
         <sections-render :props-data="apiData" :home-page="true"></sections-render>
         <app-seo-text :props-data="apiData.translate.description"></app-seo-text>
     </div>
@@ -9,9 +10,10 @@
 import { getPageBySlug } from '~/services/pageService'
 import sectionsRender from '~/components/sections-render.vue'
 import AppSeoText from '~/components/common/app-seo-text.vue'
+import AppBannersSections from '~/components/e-com/main/app-banners-sections.vue'
 export default {
     name: 'HomePage',
-    components: { sectionsRender, AppSeoText },
+    components: { sectionsRender, AppSeoText, AppBannersSections },
     async asyncData(ctx) {
         const data = await getPageBySlug(ctx)
         return {
